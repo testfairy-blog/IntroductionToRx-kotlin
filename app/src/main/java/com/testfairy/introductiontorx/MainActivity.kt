a73fa1bdefe03d167b890a8d06655b94f7c99edc
Diego Perini <diego@dperini.com>
2018-11-05 20:35:43 +0300

    Added flatMap examples

--- app/src/main/java/com/testfairy/introductiontorx/MainActivity.kt
+++ app/src/main/java/com/testfairy/introductiontorx/MainActivity.kt
@@ -3,8 +3,11 @@ package com.testfairy.introductiontorx
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.jakewharton.rxbinding3.view.clicks
@@ -17,11 +20,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        arrayExample()
-        observableExample()
+//        arrayExample()
+//        observableExample()
+//
+//        coldObservableCreate()
+//        hotObservableCreate()
 
-        coldObservableCreate()
-        hotObservableCreate()
+        incorrectHttpCallChaining()
+        correctHttpCallChaining()
     }
 
     fun arrayExample() {
@@ -113,4 +119,108 @@ class MainActivity : AppCompatActivity() {
                 { _ -> }
         )
     }
+
+    fun httpGetRx(url: String): Observable<String> {
+        val queue = Volley.newRequestQueue(this)
+
+        return Observable.create<String> { sub ->
+            val req = StringRequest(Request.Method.GET, url,
+                    Response.Listener<String> { response ->
+                        sub.onNext(response)
+                    },
+                    Response.ErrorListener { error ->
+                        sub.onError(error)
+                    })
+
+            queue.add(req)
+        }
+    }
+
+    fun httpPostRx(url: String, body: MutableMap<String, String>): Observable<String> {
+        val queue = Volley.newRequestQueue(this)
+
+        return Observable.create<String> { sub ->
+
+            val req = object : StringRequest(Request.Method.POST, url,
+                    Response.Listener<String> { response ->
+                        sub.onNext(response)
+                    },
+                    Response.ErrorListener { error ->
+                        sub.onError(error)
+                    }) {
+                override fun getParams(): MutableMap<String, String> {
+                    return body
+                }
+            }
+
+            queue.add(req)
+        }
+    }
+
+    fun correctHttpCallChaining() {
+        val loginButton = Button(this)
+        val usernameText = EditText(this)
+        val passText = EditText(this)
+
+        loginButton.clicks()
+                .flatMap {
+                    httpPostRx("https://httpbin.org/get?call=login", mutableMapOf(usernameText.text.toString() to "user", passText.text.toString() to "pass"))
+                }
+                .flatMap { _ ->
+                    httpGetRx("https://httpbin.org/get?call=getProfile")
+                            .map { profile ->
+                                listOf(profile)
+                            }
+                }
+                .flatMap { responses ->
+                    httpGetRx("https://httpbin.org/get?call=getNotifications")
+                            .map { notifications ->
+                                responses + listOf(notifications)
+                            }
+                }
+                .flatMap { responses ->
+                    httpGetRx("https://httpbin.org/get?call=getSettings")
+                            .map { settings ->
+                                responses + listOf(settings)
+                            }
+                }
+                .subscribe(
+                        { responses ->
+                            // show everything
+                        },
+                        { error ->
+                            Log.e("Rx", error.message, error)
+                        },
+                        {},
+                        { _ -> }
+                )
+    }
+
+    fun incorrectHttpCallChaining() {
+        httpPostRx("https://httpbin.org/get?call=login", mutableMapOf( "example" to "user", "123456" to "pass"))
+                .subscribe(
+                        { _ ->
+                            httpGetRx("https://httpbin.org/get?call=getProfile")
+                                    .subscribe(
+                                            { profile ->
+                                                // Show profile
+                                            },
+                                            { error ->
+                                                Log.e("Rx", error.message, error)
+                                            },
+                                            {},
+                                            { _ -> }
+                                    )
+                        },
+                        { error ->
+                            Log.e("Rx", error.message, error)
+                        },
+                        {},
+                        { _ -> }
+                )
+
+    }
 }
+
+
+
